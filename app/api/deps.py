caeeef7de6e3b52cb89834ce8ef4381fd454c5a2
Hamzaiftikhhar <hamzaiftikhhar@gmail.com>
2025-04-05 22:47:37 +0500

    Backend initialized

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,1 +1,83 @@
+from typing import Generator, Optional
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from app.db.session import SessionLocal
+from app.core.config import settings
+from app.core.security import ALGORITHM
+from app.db.models.user import User
+from app.schemas.auth import TokenPayload
+from app.crud.user import user
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/auth/login"
+)
+
+
+def get_db() -> Generator:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+) -> User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    current_user = user.get(db, id=token_data.sub)
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    return current_user
+
+
+def get_current_active_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    if not user.is_active(current_user):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user"
+        )
+    return current_user
+
+
+def get_current_verified_user(
+    current_user: User = Depends(get_current_active_user),
+) -> User:
+    if not user.is_verified(current_user):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User email not verified"
+        )
+    return current_user
+
+
+def get_current_superuser(
+    current_user: User = Depends(get_current_active_user),
+) -> User:
+    if not user.is_superuser(current_user):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not enough permissions"
+        )
+    return current_user
